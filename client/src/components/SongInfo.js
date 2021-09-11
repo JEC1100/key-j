c025d676e62e99eb48a6d9b10886c2a0db418e10
Pav <pav0107@gmail.com>
2021-09-11 16:09:09 +0100

    removed unnecessary comment

--- client/src/components/SongInfo.js
+++ client/src/components/SongInfo.js
@@ -3,7 +3,6 @@ import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 
 import { v4 as uuidv4 } from 'uuid';
-// run npm install uuid from command line
 
 const SongInfo = (props) => {
   const [initialState, setInitialState] = useState([]);
