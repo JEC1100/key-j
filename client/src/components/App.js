2831d83bb3e6e163e7d5c5620857f91b6011d660
Pav <pav0107@gmail.com>
2021-09-12 11:10:42 +0100

    shortened return in App.js through use of ternary

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,27 +9,30 @@ const App = () => {
     setSongTitle(songname);
   };
 
-  if (songTitle) {
-    return (
-      <div className="app">
-        <h1>Key-J</h1>
-        <div className="searchbar">
-          <Searchbar submit={submitSongName} />
-        </div>
-        <div className="info-table">{<SongInfo songTitle={songTitle} />}</div>
+  // if (songTitle) {
+  return (
+    <div className="app">
+      <h1>Key-J</h1>
+      <div className="searchbar">
+        <Searchbar submit={submitSongName} />
       </div>
-    );
-  } else {
-    return (
-      <div className="app">
-        <h1>Key-J</h1>
-        <div className="searchbar">
-          <Searchbar submit={submitSongName} />
-        </div>
-        <div className="info-table"></div>
+      <div className="info-table">
+        {songTitle ? <SongInfo songTitle={songTitle} /> : null}
       </div>
-    );
-  }
+    </div>
+  );
+  // }
+  // else {
+  //   return (
+  //     <div className="app">
+  //       <h1>Key-J</h1>
+  //       <div className="searchbar">
+  //         <Searchbar submit={submitSongName} />
+  //       </div>
+  //       <div className="info-table"></div>
+  //     </div>
+  //   );
+  // }
 };
 
 export default App;
--- client/src/components/SongInfo.js
+++ client/src/components/SongInfo.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
-
 import { v4 as uuidv4 } from 'uuid';
 
 const SongInfo = (props) => {
