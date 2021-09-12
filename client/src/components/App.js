8ec6b0fb93c46401b87ba14dfd054ac1c20cad42
Pav <pav0107@gmail.com>
2021-09-12 12:13:57 +0100

    updated return in App.js

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -9,7 +9,6 @@ const App = () => {
     setSongTitle(songname);
   };
 
-  // if (songTitle) {
   return (
     <div className="app">
       <h1>Key-J</h1>
@@ -21,18 +20,6 @@ const App = () => {
       </div>
     </div>
   );
-  // }
-  // else {
-  //   return (
-  //     <div className="app">
-  //       <h1>Key-J</h1>
-  //       <div className="searchbar">
-  //         <Searchbar submit={submitSongName} />
-  //       </div>
-  //       <div className="info-table"></div>
-  //     </div>
-  //   );
-  // }
 };
 
 export default App;
