a5174e5582c81910c66e63eedd337aac63f35757
lushcodes <lushcodes@gmail.com>
2021-09-13 15:47:11 +0100

    Consolidated routes and logic for get_multiple_audio_features

--- server/src/api/get_multiple_audio_features.js
+++ server/src/api/get_multiple_audio_features.js
@@ -1,14 +1,14 @@
 require('dotenv').config();
 
 let SpotifyWebApi = require('spotify-web-api-node');
-let compareSongs = require('../compare_songs/compare_songs');
+// let compareSongs = require('../compare_songs/compare_songs');
 
 let spotifyApi = new SpotifyWebApi({
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
 });
 
-const compareAudioFeatures = (songOneId, songTwoId) => {
+const getMultipleAudioFeatures = (songOneId, songTwoId) => {
   spotifyApi.clientCredentialsGrant().then(
     function(data) {
       spotifyApi.setAccessToken(data.body['access_token']);
@@ -18,15 +18,16 @@ const compareAudioFeatures = (songOneId, songTwoId) => {
     })
     .then(function(data) {
       let result = data.body.audio_features;
-      let comparison = compareSongs(result[0], result[1]);
-      return comparison;
+      // let comparison = compareSongs(result[0], result[1]);
+      console.log(result);
+      return result;
     },
     function(err) {
       console.log('Something went wrong!', err);
     });
 };
 
-compareAudioFeatures('4iV5W9uYEdYUVa79Axb7Rh', '3Qm86XLflmIXVm1wcwkgDK');
+getMultipleAudioFeatures('4iV5W9uYEdYUVa79Axb7Rh', '3Qm86XLflmIXVm1wcwkgDK');
 
 
 // ['4iV5W9uYEdYUVa79Axb7Rh', '3Qm86XLflmIXVm1wcwkgDK']
