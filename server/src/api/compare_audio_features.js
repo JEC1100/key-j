a50fdee985c7c2c3fcc6096460121f558ab4126c
lushcodes <lushcodes@gmail.com>
2021-09-13 12:44:08 +0100

    Altered compare_audio_feature.js - changed to output raw similarity data

--- server/src/api/compare_audio_features.js
+++ server/src/api/compare_audio_features.js
@@ -1,29 +1,32 @@
 require('dotenv').config();
 
 let SpotifyWebApi = require('spotify-web-api-node');
-let compareSongs = require('../compare_songs/compare_audio_features');
+let compareSongs = require('../compare_songs/compare_songs');
 
 let spotifyApi = new SpotifyWebApi({
   clientId: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
 });
 
-const compareAudioFeatures = () => {
+const compareAudioFeatures = (songOneId, songTwoId) => {
   spotifyApi.clientCredentialsGrant().then(
     function(data) {
       spotifyApi.setAccessToken(data.body['access_token']);
       return spotifyApi.getAudioFeaturesForTracks(
-        ['4iV5W9uYEdYUVa79Axb7Rh', '3Qm86XLflmIXVm1wcwkgDK']
+        [songOneId, songTwoId]
       );
     })
     .then(function(data) {
       let result = data.body.audio_features;
       let comparison = compareSongs(result[0], result[1]);
-      console.log(comparison);
+      return comparison;
     },
     function(err) {
       console.log('Something went wrong!', err);
     });
 };
 
-compareAudioFeatures();
+compareAudioFeatures('4iV5W9uYEdYUVa79Axb7Rh', '3Qm86XLflmIXVm1wcwkgDK');
+
+
+// ['4iV5W9uYEdYUVa79Axb7Rh', '3Qm86XLflmIXVm1wcwkgDK']
