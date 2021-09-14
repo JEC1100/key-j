de9916aae7b4bcfa85f95fecc00a852daae80733
Matt Firmston <mattfirmston@gmail.com>
2021-09-14 14:22:39 +0100

    connected jest-puppeteer to run UI tests, and created and passed first two tests

--- server/controllers/root_controller.js
+++ server/controllers/root_controller.js
@@ -1,6 +1,7 @@
 const getTracks = require('../src/api/get_tracks');
 const audioFeatures = require('../src/api/get_track_audio_features');
 const songsLikeThis = require('../src/api/get_songs_like_this');
+// eslint-disable-next-line max-len
 const getMultipleAudioFeatures = require('../src/api/get_multiple_audio_features');
 
 exports.root = (req, res) => {
@@ -23,9 +24,9 @@ exports.songsLikeThis = (req, res) => {
   songsLikeThis(req.body.song).then(data => {
     res.json({
       songs: data,
-    })
-  })
-}
+    });
+  });
+};
 
 exports.compare = (req, res) => {
   let request = req.body.songId;
