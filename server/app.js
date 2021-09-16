4807b5583581e06b53f23af87e4e62a9365b2f65
Isobel <isobelpeppiatt@yahoo.co.uk>
2021-09-16 16:55:09 +0100

    modify index path in app.js

--- server/app.js
+++ server/app.js
@@ -9,9 +9,8 @@ app.use('/api/', require('./routes/root'));
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', '../client/public/index.html'));
 });
 
 const PORT = process.env.PORT || 3001;
