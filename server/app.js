2d97e15eb0ecfc2bdfbbda1e69ed5c97aea01556
Isobel <isobelpeppiatt@yahoo.co.uk>
2021-09-16 17:00:46 +0100

    add path to build for static files

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 
 app.use('/api/', require('./routes/root'));
 
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', '../client/build/index.html'));
